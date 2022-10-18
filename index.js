8764718b598986aed3a649cbb531caac3cb7cc6d
krst221 <krst221@gmail.com>
2022-10-18 16:21:06 +0200

    proyecto node

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const {connect} = require('./src/utils/db');
+const {isAuth} = require('./src/middlewares/auth');
+
+const gimnasioRouter = require('./src/api/routes/gimnasio.routes');
+const pokemonRouter = require('./src/api/routes/pokemon.route');
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+const app = express();
+connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/gimnasios', gimnasioRouter);
+app.use('/pokemons', pokemonRouter);
+
+app.listen(PORT, () => console.log(`listening on port: http://localhost:${PORT}`));
