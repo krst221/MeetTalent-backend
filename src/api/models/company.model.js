7dee287199b806b690d95f63bad9938761dc5694
krst221 <krst221@gmail.com>
2022-11-26 09:23:29 +0100

    img empresa

--- src/api/models/company.model.js
+++ src/api/models/company.model.js
@@ -9,6 +9,7 @@ const CompanySchema = new Schema({
 
     name: {type: String, required: true},
     ID: {type: String, required: true},
+    picture: {type: String, required: true, default: 'https://happytravel.viajes/wp-content/uploads/2020/04/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'},
     email: {type: String, required: true},
     password: {type: String, required: true},
     offers: [{type: Schema.Types.ObjectId, ref: "Offer"}],
