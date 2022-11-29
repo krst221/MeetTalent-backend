302786f5c72f787cb6a9898111447c9549129acd
krst221 <krst221@gmail.com>
2022-11-29 20:06:42 +0100

    vercel

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Method', 'POST, GET, DELETE, PUT, PATCH');
     res.header('Access-Control-Allow-Credentials', 'false');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 })
 
